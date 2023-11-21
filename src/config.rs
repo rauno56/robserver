26278c5dae9302666a2e8cf79fbf07ad7081861c
Rauno Viskus <1334536+rauno56@users.noreply.github.com>
2023-11-21 14:16:26 +0200

    feat: do inserts and updates in one query (#3)
    * feat: insert entities and update counts in one query
    
    * docs: add comment about permissions needed to set up registry access
    
    * feat: add a deno script to publish random messages
    
    * feat: add max query size parameter to let buffer be bigger than the query size
    
    * fix: remove sleep from the consumer
    
    * style: fmt
    
    * feat: add ref name to tags

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,7 @@ pub fn init() {
 }
 
 pub fn get_buffer_size() -> usize {
-	std::env::var("ROBSERVER_BUFFER_SIZE").map_or(1_000, |v| {
+	std::env::var("ROBSERVER_BUFFER_SIZE").map_or(10_000, |v| {
 		v.parse::<usize>().expect("invalid ROBSERVER_BUFFER_SIZE")
 	})
 }
@@ -25,8 +25,15 @@ pub mod amqp {
 	}
 
 	pub fn get_prefetch() -> u16 {
-		std::env::var("ROBSERVER_PREFETCH").map_or(10, |v| {
+		std::env::var("ROBSERVER_PREFETCH").map_or(100, |v| {
 			v.parse::<u16>().expect("invalid ROBSERVER_PREFETCH")
+		})
+	}
+
+	pub fn get_queue_max_length() -> u32 {
+		std::env::var("ROBSERVER_QUEUE_MAX_LENGTH").map_or(100_000, |v| {
+			v.parse::<u32>()
+				.expect("invalid ROBSERVER_QUEUE_MAX_LENGTH")
 		})
 	}
 }
@@ -35,5 +42,12 @@ pub mod psql {
 	pub fn get_url() -> String {
 		std::env::var("ROBSERVER_PG_ADDR")
 			.unwrap_or_else(|_| "postgres://postgres@127.0.0.1/robserver".into())
+	}
+
+	pub fn get_max_query_size() -> usize {
+		std::env::var("ROBSERVER_MAX_QUERY_SIZE").map_or(1_000, |v| {
+			v.parse::<usize>()
+				.expect("invalid ROBSERVER_MAX_QUERY_SIZE")
+		})
 	}
 }
