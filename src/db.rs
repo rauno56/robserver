1c5d01451f2a44755e1cefc5b287f9210341c023
Rauno Viskus <rauno56@gmail.com>
2023-12-28 08:56:21 +0200

    chore: remove dead code

--- src/db.rs
+++ src/db.rs
@@ -1,84 +1,12 @@
 use serde_json::Value;
 use sqlx::postgres::PgQueryResult;
 use sqlx::{types::BigDecimal, PgPool};
-use std::error::Error;
 use tokio::sync::mpsc;
 use tracing::info;
 
 use crate::config;
 use crate::payload::{Payload, Data};
 use std::collections::HashMap;
-
-#[allow(dead_code)]
-async fn record(
-	conn: &PgPool,
-	id: u64,
-	vhost: &str,
-	exchange: &str,
-	json: &Value,
-) -> Result<PgQueryResult, sqlx::Error> {
-	sqlx::query!(
-		r#"
-		insert into data.entity as e (
-			id, vhost, exchange, payload
-		) values (
-			$1, $2, $3, $4
-		)
-		on conflict
-			on constraint entity_pkey
-				do update set count = e.count + 1, last_seen_at = now()
-	"#,
-		BigDecimal::from(id),
-		vhost,
-		exchange,
-		json,
-	)
-	.execute(conn)
-	.await
-}
-
-#[allow(dead_code)]
-async fn update_counts(
-	conn: &PgPool,
-	mut counts: HashMap<Payload, usize>,
-) -> Result<PgQueryResult, sqlx::Error> {
-	let mut id = Vec::with_capacity(counts.len());
-	let mut vhost = Vec::with_capacity(counts.len());
-	let mut exchange = Vec::with_capacity(counts.len());
-	let mut count = Vec::with_capacity(counts.len());
-	for (p, to_add) in counts.drain() {
-		if to_add == 0 {
-			continue;
-		}
-		id.push(BigDecimal::from(p.id));
-		vhost.push(p.vhost);
-		exchange.push(p.exchange);
-		count.push(to_add as i32);
-	}
-	info!("Updating counts for {:?} items", id.len());
-	sqlx::query!(
-		r#"
-		update data.entity as e
-		set
-			count = count + new.add,
-			last_seen_at = now()
-		from (
-			select
-				unnest($1::numeric[]) as id,
-				unnest($2::text[]) as vhost,
-				unnest($3::text[]) as exchange,
-				unnest($4::integer[]) as add
-		) as new
-		where (e.id, e.vhost, e.exchange) = (new.id, new.vhost, new.exchange)
-	"#,
-		&id[..],
-		&vhost[..],
-		&exchange[..],
-		&count[..],
-	)
-	.execute(conn)
-	.await
-}
 
 async fn insert_counts(
 	conn: &PgPool,
