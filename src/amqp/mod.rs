711eb29582b267873e2a018a938c2bf7f0c60f84
Rauno Viskus <rauno56@gmail.com>
2024-01-18 21:44:51 +0200

    feat: bind to direct exchanges

--- src/amqp/mod.rs
+++ src/amqp/mod.rs
@@ -8,7 +8,7 @@ use lapin::{options::*, types::FieldTable, Channel, Connection, ConnectionProper
 use tokio::sync::mpsc;
 use tokio::time;
 use tokio::time::{timeout, Duration};
-use tracing::{error, info};
+use tracing::{debug, error, info};
 
 use crate::config::amqp as config;
 use crate::payload::Payload;
@@ -74,33 +74,45 @@ pub async fn payload_parser(payloads: mpsc::Sender<Payload>, channel: Channel) {
 	}
 }
 
+#[derive(PartialEq, Eq, Hash, Debug)]
+struct BindableEx {
+	pub name: String,
+	pub routing_key: String,
+}
+
 struct Binder {
 	connection: Connection,
 	channel: Channel,
 	queue: String,
-	_bound: HashSet<()>,
+	bound: HashSet<BindableEx>,
 }
 
 impl Binder {
 	pub async fn new(connection: Connection, queue: String) -> Self {
 		let channel = connection.create_channel().await.expect("create_channel");
 
-		Binder { connection, channel, queue, _bound: HashSet::new() }
+		Binder { connection, channel, queue, bound: HashSet::new() }
 	}
 
-	pub async fn bind(&mut self, ex: &str, routing_key: &str) {
+	pub async fn bind(&mut self, bindable: BindableEx) {
+		if self.bound.contains(&bindable) {
+			debug!(?bindable, "Already bound");
+			return
+		}
+
 		match self.channel
 			.queue_bind(
 				&self.queue,
-				ex,
-				routing_key,
+				&bindable.name,
+				&bindable.routing_key,
 				QueueBindOptions::default(),
 				FieldTable::default(),
 			)
 			.await
 		{
 			Ok(_) => {
-				info!(exchange = ex, "Successfully bound")
+				info!(?bindable, "Successfully bound");
+				self.bound.insert(bindable);
 			}
 			Err(_) => {
 				self.channel = self.connection.create_channel().await.unwrap();
@@ -118,10 +130,13 @@ pub async fn exchange_subscriber(conn: Connection) {
 		info!("No exchanges to bind to");
 	} else {
 		for ex in exchanges {
-			binder.bind(ex.as_str(), "#").await;
+			binder.bind(BindableEx { name: ex, routing_key: "#".to_string() } ).await;
 		}
 	}
 
+	// TODO: match on vhost
+	const VHOST: &str = "/";
+	const Q: &str = "queue";
 	let definitions_url = config::get_definitions_url();
 	let queue_name = config::get_queue();
 	let mut interval = time::interval(Duration::from_millis(5000));
@@ -129,16 +144,38 @@ pub async fn exchange_subscriber(conn: Connection) {
 	loop {
 			interval.tick().await;
 			match get_definitions(&definitions_url).await {
-				Ok(result) => {
-					for ex in result.exchanges {
-						println!("bind to exchange: {:?}", ex);
+				Ok(mut result) => {
+					result.bindings.retain(|binding| binding.vhost == VHOST && !(binding.destination_type == Q && binding.destination == queue_name));
+
+					for ex_index in 0..result.exchanges.len() {
+						let ex = &result.exchanges[ex_index];
+						if ex.vhost != VHOST {
+							continue;
+						}
+						match ex.r#type {
+							ExchangeType::Direct => {
+								let mut found_count = 0;
+								for binding_index in 0..result.bindings.len() {
+									let binding = &result.bindings[binding_index];
+									if &binding.source == &ex.name {
+										binder.bind(BindableEx { name: ex.name.clone(), routing_key: binding.routing_key.clone() }).await;
+										found_count += 1;
+									}
+								}
+								if found_count == 0 {
+									// Change to debug
+									debug!(exchange=ex.name, "No bindings found", );
+								} else {
+									// Change to debug
+									debug!(found_count, name=ex.name, "Bindings found");
+								}
+							}
+							_ => {
+								binder.bind(BindableEx { name: ex.name.clone(), routing_key: "#".to_string() }).await;
+							}
+						};
 					}
 
-					for binding in result.bindings {
-						if binding.destination != queue_name {
-							println!("bind to exchange: {:?}", binding);
-						}
-					}
 				},
 				Err(error) => {
 					error!(error, "Failed to get definitions");
--- src/amqp/types.rs
+++ src/amqp/types.rs
@@ -1,4 +1,5 @@
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use serde::de;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Binding {
@@ -10,11 +11,48 @@ pub struct Binding {
 	// arguments: ??,
 }
 
+#[derive(Debug)]
+pub enum ExchangeType {
+	Direct,
+	Headers,
+	Topic,
+	Fanout,
+}
+
+impl Serialize for ExchangeType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(match *self {
+            ExchangeType::Direct => "direct",
+            ExchangeType::Headers => "headers",
+            ExchangeType::Topic => "topic",
+            ExchangeType::Fanout => "fanout",
+            // ExchangeType::Other(ref other) => other,
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for ExchangeType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>
+    {
+        let s = String::deserialize(deserializer)?;
+        match s.as_str() {
+            "direct" => Ok(ExchangeType::Direct),
+            "headers" => Ok(ExchangeType::Headers),
+            "topic" => Ok(ExchangeType::Topic),
+            "fanout" => Ok(ExchangeType::Fanout),
+            _ => Err(de::Error::custom(format!("Invalid exchange type: {}", s))),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Exchange {
 	pub name: String,
 	pub vhost: String,
-	pub r#type: String,
+	pub r#type: ExchangeType,
 	pub auto_delete: bool,
 	pub internal: bool,
 	// arguments: ??,
